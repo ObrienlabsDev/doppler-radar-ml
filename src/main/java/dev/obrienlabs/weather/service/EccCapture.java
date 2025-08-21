aec2d25e23bbd478fa96dacea6a02ec34920b59f
Michael O'Brien <michael@obrienlabs.org>
2025-08-21 12:27:04 -0400

    #3 - spring boot start

---
+++ src/main/java/dev/obrienlabs/weather/service/EccCapture.java
@@ -0,0 +1,11 @@
+package dev.obrienlabs.weather.service;
+
+public class EccCapture {
+	
+
+	public static void main(String[] argv) {
+	
+		EccCapture eccCapture = new EccCapture();
+		System.out.println(eccCapture);
+	}
+}
---
+++ src/test/java/dev/obrienlabs/weather/service/EccCaptureTest.java
@@ -0,0 +1,11 @@
+package dev.obrienlabs.weather.service;
+
+public class EccCaptureTest {
+	
+
+	public static void main(String[] argv) {
+	
+		EccCaptureTest eccCaptureTest = new EccCaptureTest();
+		System.out.println(eccCaptureTest);
+	}
+}
